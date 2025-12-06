ab425e938ba54173ff354d86d22c3d3648eaad6d
bituan <113036994+bituann@users.noreply.github.com>
2025-12-06 01:15:16 +0100

    initial commit

---
+++ src/main/java/com/bituan/ai_rag_document_search/AiRagDocumentSearchApplication.java
@@ -0,0 +1,13 @@
+package com.bituan.ai_rag_document_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRagDocumentSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRagDocumentSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bituan/ai_rag_document_search/AiRagDocumentSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bituan.ai_rag_document_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRagDocumentSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
